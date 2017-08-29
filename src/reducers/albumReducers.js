3eb22bcc927318df7b5d387e1e96b491c48bc865
Ankur Patel <pateal7014@gmail.com>
2017-08-29 15:11:04 -0500

    Update Navbar

--- src/reducers/albumReducers.js
+++ src/reducers/albumReducers.js
@@ -5,7 +5,7 @@ export function albumReducers(state={ albums: []}, action){
     let allAlbums = [...state.albums];
     switch (action.type){
         case 'GET_ALBUMS':
-            return {albums :[...state.albums, ...action.payload]}
+            return {albums :[...action.payload]}
             break;
     }
     return state;
